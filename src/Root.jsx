2865b53c2fd3b946476d3f216c4ed7afe7909f00
tanbir12380 <mdkamrulhasantanbir0001@gmail.com>
2025-11-29 08:35:49 +0600

    final touch

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,14 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Header from "./header";
 import { Outlet } from "react-router";
 import Footer from "./Footer";
 import { ToastContainer } from "react-toastify";
 
 const Root = () => {
-  useEffect(() => {
-    document.body.classList.add("theme-light");
-  }, []);
-
   return (
     <div
       style={{
